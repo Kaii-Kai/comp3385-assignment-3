ab7bea4e93eb45a2857f68f09816c816cfa23c10
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2025-03-18 16:09:54 -0400

    Modified file

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function create() {
+		return view('login-form');
+	}
+	
+	public function store(Request $request): RedirectResponse{
+		$credentials = $request->validate([
+			'email'=> 'required|email',
+			'password'=> 'required',
+		]);
+		
+		if (Auth::attempt($credentials)) {
+			return redirect()->route('dashboard')->with('success', 'Login successful');
+		}
+		
+		return back()->withErrors(['email' => 'Invalid credentials. Check the email adress and password entered.']);
+	}
+	
+}
+	
