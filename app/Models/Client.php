4bc32e5aaf7a645e746743154467432072d1fbec
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2025-03-18 19:33:51 -0400

    Modified file

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+	
+	protected $table = 'clients';
+	
+	protected $fillable = [
+		'name',
+		'email',
+		'phone',
+		'address',
+		'company_logo'
+	];
+	
+	public $timestamps = true;
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="display-5 fw-bold text-body-emphasis">Dashboard</h1>
-    <p class="lead">Welcome to your dashboard. Here you can manage your account, your clients and much more.</p>
+	 <div class="d-flex justify-content-between align-items-center">
+        <h1 class="display-5 fw-bold text-body-emphasis">Dashboard</h1>
+		
+        <a href="/clients/add" class="btn btn-primary ml-auto">+ Create Client</a>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Telephone</th>
+                    <th>Address</th>
+                    <th>Company Logo</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($clients as $client)
+                    <tr>
+                        <td>{{ $client->name }}</td>
+                        <td>{{ $client->email }}</td>
+                        <td>{{ $client->telephone }}</td>
+                        <td>{{ $client->address }}</td>
+                        <td>
+                            @if ($client->company_logo)
+                                <img src="{{ asset('storage/' . $client->company_logo) }}" alt="Company Logo" width="100" height="100">
+                            @else
+                                No Logo
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
